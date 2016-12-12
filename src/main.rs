c191dea3e665962987039ccfd364d9d2ea566a44
Armin Häberling <armin.aha@gmail.com>
2016-12-12 20:26:12 +0100

    Download an url given on the command line

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+extern crate hyper;
+
+use hyper::Client;
+use hyper::Url;
+
+use std::io::Read;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    println!("Downloading {}", args[1]);
+    let url = Url::parse(&args[1]).unwrap();
+    let client = Client::new();
+    let mut res = client.get(url).send().unwrap();
+    println!("{:?}", res);
+    let mut buffer = String::new();
+    res.read_to_string(&mut buffer).unwrap();
+    println!("{}", buffer);
 }
