903a32798dc068d33bcd0f02fa19c718206882a9
Armin Häberling <armin.aha@gmail.com>
2016-12-20 21:46:32 +0100

    Add some error handling

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 extern crate hyper;
 
 use hyper::Client;
-use hyper::error::Result;
+use hyper::error::{Error, Result};
 use hyper::Url;
 
 use std::io::Read;
@@ -18,15 +18,18 @@ fn main() {
 
     println!("Scan Status of {}", &host);
     let client = Client::new();
-    let status = get_scan_status(&client, &host).unwrap();
+    let status = get_scan_status(&client, &host)
+                    .unwrap_or_else(|err| "Error: ".to_string() + &err.to_string());
     println!("{}", &status);
 }
 
 fn get_scan_status(client: &Client, host: &str) -> Result<String> {
     let url = "http://".to_string() + host + "/Scan/Status";
-    let url = Url::parse(&url).unwrap();
-    let mut response = client.get(url).send().unwrap();
+    let url = try!(Url::parse(&url));
+    let mut response = try!(client.get(url).send());
     let mut buffer = String::new();
-    response.read_to_string(&mut buffer).unwrap();
-    Ok(buffer)
+    match response.read_to_string(&mut buffer) {
+        Ok(_) => Ok(buffer),
+        Err(e) => Err(Error::Io(e))
+    }
 }
