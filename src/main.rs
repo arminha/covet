52143859caffd676d84a8044f18a255c5d76e33a
Armin Häberling <armin.aha@gmail.com>
2016-12-20 21:22:54 +0100

    Print scan status

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,32 @@
 extern crate hyper;
 
 use hyper::Client;
+use hyper::error::Result;
 use hyper::Url;
 
 use std::io::Read;
 use std::env;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let host = match env::args().nth(1) {
+        Some(host) => host,
+        None => {
+            println!("Usage: covet <host>");
+            return;
+        }
+    };
 
-    println!("Downloading {}", args[1]);
-    let url = Url::parse(&args[1]).unwrap();
+    println!("Scan Status of {}", &host);
     let client = Client::new();
-    let mut res = client.get(url).send().unwrap();
-    println!("{:?}", res);
+    let status = get_scan_status(&client, &host).unwrap();
+    println!("{}", &status);
+}
+
+fn get_scan_status(client: &Client, host: &str) -> Result<String> {
+    let url = "http://".to_string() + host + "/Scan/Status";
+    let url = Url::parse(&url).unwrap();
+    let mut response = client.get(url).send().unwrap();
     let mut buffer = String::new();
-    res.read_to_string(&mut buffer).unwrap();
-    println!("{}", buffer);
+    response.read_to_string(&mut buffer).unwrap();
+    Ok(buffer)
 }
