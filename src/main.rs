ed24441a73f70611bc524d09a83b228257028183
Armin Häberling <armin.aha@gmail.com>
2017-01-04 16:20:28 +0100

    Some refactoring in main method

--- src/main.rs
+++ src/main.rs
@@ -21,23 +21,8 @@ fn main() {
         }
     };
 
-    println!("Scan Status of {}", &host);
     let client = Client::new();
-    let xml = match get_scan_status(&client, &host) {
-        Ok(xml) => xml,
-        Err(e) => {
-            println!("Error: {}", &e);
-            return;
-        }
-    };
-    let status = match ScanStatus::read_xml(xml) {
-        Ok(status) => status,
-        Err(e) => {
-            println!("Error: Could not parse XML: {}", e);
-            return;
-        }
-    };
-    println!("scanner: {:?}, adf: {:?}", status.get_scanner_state(), status.get_adf_state());
+    print_scan_status(&client, &host);
 
     let job = ScanJob::new(InputSource::Platen, true, Format::Pdf, ColorSpace::Color);
     let mut target: Vec<u8> = Vec::new();
@@ -46,7 +31,25 @@ fn main() {
     println!("{}", result);
 }
 
-fn get_scan_status(client: &Client, host: &str) -> HResult<Response> {
+fn print_scan_status(client: &Client, host: &str) {
+    println!("Scan Status of {}", &host);
+    let status = match get_scan_status(&client, &host) {
+        Ok(status) => status,
+        Err(e) => {
+            println!("Error: {}", e);
+            return;
+        }
+    };
+    println!("Scanner: {:?}, Adf: {:?}", status.get_scanner_state(), status.get_adf_state());
+}
+
+fn get_scan_status(client: &Client, host: &str) -> Result<ScanStatus, String> {
+    retrieve_scan_status(&client, &host)
+        .map_err(|e| e.to_string())
+        .and_then(ScanStatus::read_xml)
+}
+
+fn retrieve_scan_status(client: &Client, host: &str) -> HResult<Response> {
     let url = "http://".to_string() + host + "/Scan/Status";
     let url = try!(Url::parse(&url));
     client.get(url).send()
